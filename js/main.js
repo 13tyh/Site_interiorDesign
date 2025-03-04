e86bf4be97e9da86a0975d040a0180ae49bce4fa
13tyh <yuck5g@gmail.com>
2025-03-04 15:42:37 +0900

    productsページ作成

--- js/main.js
+++ js/main.js
@@ -46,3 +46,44 @@ $(function () {
     $(this).addClass("inview");
   });
 });
+
+// プロダクトのタブ切り替え
+$(function () {
+  $(".tab-list .tab-all").addClass("active");
+  $(".products-list all").addClass("active");
+
+  $(".tab-all").click(function () {
+    $(".tab-list li").removeClass("active");
+    $(".products-list").removeClass("active");
+    $(this).addClass("active");
+    $(".products-list.all").addClass("active");
+  });
+
+  $(".tab-sofa").click(function () {
+    $(".tab-list li").removeClass("active");
+    $(".products-list").removeClass("active");
+    $(this).addClass("active");
+    $(".products-list.sofa").addClass("active");
+  });
+
+  $(".tab-desk").click(function () {
+    $(".tab-list li").removeClass("active");
+    $(".products-list").removeClass("active");
+    $(this).addClass("active");
+    $(".products-list.desk").addClass("active");
+  });
+
+  $(".tab-chair").click(function () {
+    $(".tab-list li").removeClass("active");
+    $(".products-list").removeClass("active");
+    $(this).addClass("active");
+    $(".products-list.chair").addClass("active");
+  });
+
+  $(".tab-dining").click(function () {
+    $(".tab-list li").removeClass("active");
+    $(".products-list").removeClass("active");
+    $(this).addClass("active");
+    $(".products-list.dining").addClass("active");
+  });
+});
